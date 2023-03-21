7e824790106ab6fc04d77b262162b889e2439b3e
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2023-03-21 09:00:07 +0100

    fix a few bugs: added try catch, fix the AddTime method,refactor contructor Time used a default value

--- Time/Program.cs
+++ Time/Program.cs
@@ -3,86 +3,61 @@ public class Time
 {
     private int hours;
     private int minutes;
-    private int seconds;
+    private int seconds ;
 
-        public Time(int hours, int minutes, int seconds)
+    public Time(int hours, int minutes, int seconds)
     {
-       
         Hours = hours;
         Minutes = minutes;
         Seconds = seconds;
     }
-    public Time()
+    public Time() : this(12, 11, 21)
     {
-        Seconds = 30;
-        Minutes = 32;
-        Hours = 21;
-    }
 
+    }
     public int Hours
     {
-        get { return hours; }
+        get => hours; 
         set
         {
-            try
-            {
 
-                if (value < 0 || value > 23)
-                    throw new ArgumentException("Invalid minutes value");
-                hours = value;
-
-            }
-            catch (Exception Ex)
+            if (value < 0 || value > 23)
             {
-                Console.WriteLine(Ex.Message, "Try again: \n");
-                value = Convert.ToInt32(Console.ReadLine());
-                hours = value;
+                throw new ArgumentException("Invalid hours value");    
             }
+            hours = value;
+
         }
       }
 
     public int Minutes
     {
-        get { return minutes; }
+
+        get => minutes;
         set
         {
-            try
-            {
 
             if (value < 0 || value > 59)
+            {
                 throw new ArgumentException("Invalid minutes value");
-                    minutes = value;
-                
             }
-            catch(Exception Ex) {
-                Console.WriteLine(Ex.Message,"Try again: \n");
-                value = Convert.ToInt32(Console.ReadLine());
-                minutes = value;
-            }
-            
+            minutes = value;
            
         }
     }
 
     public int Seconds
     {
-        get { return seconds; }
+        get => seconds;
         set
         {
-            try
-            {
-
+            
                 if (value < 0 || value > 59)
-                    throw new ArgumentException("Invalid minutes value");
+                {
+                    throw new ArgumentException("Invalid seconds value");
+                }
                 seconds = value;
 
-            }
-            catch (Exception Ex)
-            {
-                Console.WriteLine(Ex.Message, "Try again: \n");
-                value = Convert.ToInt32(Console.ReadLine());
-                seconds = value;
-            }
         }
     }
 
@@ -92,38 +67,48 @@ public class Time
         if (this.seconds >= 60)
         {
             this.minutes += this.seconds / 60;
-            this.seconds -= 60;
+            this.seconds %= 60;
         }
        this.minutes += minutes ;
         if (this.minutes >= 60)
         {
             this.hours += this.minutes / 60;
-            this.minutes -= 60;
+            this.minutes %= 60;
         }
         this.hours += hours;
-         if(this.hours>=24)
-            {
-             this.hours -= 24;
-            }
-
-      
+        if  (this.hours >= 24)
+        {
+             this.hours %= 24;
+        }
     }
     public override string ToString()
     {
-        return $"\nSeconds: {Seconds}, Minutes: {Minutes}, Hours: {Hours}";
+        return $"Seconds: {Seconds::00}, Minutes: {Minutes::00}, Hours: {Hours::00}";
     }
     static void Main(string[] args)
     {
-        Time time1 = new Time(21,23,21);
-        Time time2 = new Time(25, 23, 20);
-        Time time3 = new Time();
-        time1.AddTime(1,30,1);
-        time2.AddTime(1, 40, 1);
-        time3.AddTime(2, 30, 34);
-        Console.WriteLine(time1.ToString());
-        Console.WriteLine(time2.ToString());
-        Console.WriteLine(time3.ToString());
-        Console.ReadLine();
+        Time time = null, time1 = null;
+        try
+        {
+             time = new Time(21, 23, 21);
+             time1 = new Time(25, 23, 20);
+        }
+        catch(ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        try
+        {
+            time.AddTime(1, 30, 1);
+            time1.AddTime(1, 40, 1);
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        Console.WriteLine(time);
+        Console.WriteLine(time1);
+        Console.ReadKey();
     }
 }
 
